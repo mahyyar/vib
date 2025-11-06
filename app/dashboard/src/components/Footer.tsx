1844b94ca68be7b8cda53cd3b6b6fb4b41d48383
MahYaR <mohammadetayy@gmail.com>
2025-11-07 01:04:08 +0330

    Simplify footer component and update text
    Removed version display and links to organization and repository, and updated footer text.

--- app/dashboard/src/components/Footer.tsx
+++ app/dashboard/src/components/Footer.tsx
@@ -1,10 +1,7 @@
-import { BoxProps, HStack, Link, Text } from "@chakra-ui/react";
-import { ORGANIZATION_URL, REPO_URL } from "constants/Project";
-import { useDashboard } from "contexts/DashboardContext";
+import { BoxProps, HStack, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
 export const Footer: FC<BoxProps> = (props) => {
-  const { version } = useDashboard();
   return (
     <HStack w="full" py="0" position="relative" {...props}>
       <Text
@@ -14,14 +11,10 @@ export const Footer: FC<BoxProps> = (props) => {
         color="gray.500"
         fontSize="xs"
       >
-        <Link color="blue.400" href={REPO_URL}>
-          Marzban
-        </Link>
-        {version ? ` (v${version}), ` : ", "}
-        Made with ❤️ in{" "}
-        <Link color="blue.400" href={ORGANIZATION_URL}>
-          Gozargah
-        </Link>
+        Forked by Mahyar{" "}
+        <Text as="span" color="red.500" fontWeight="bold">
+          ❤️
+        </Text>
       </Text>
     </HStack>
   );
